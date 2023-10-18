5d3039fc53c85c7359e1dcefc51b3818e0844e29
zhyk <zhyk@list.ru>
2023-10-19 00:13:43 +0300

    Первая задача

---
+++ Task01/Program.cs
@@ -0,0 +1,5 @@
+﻿// Принимает на вход трехзначное число и на выходе показывает вторую цифру.
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int SecondDigit = (number % 100 - number % 10) / 10;
+Console.Write($"Вторая цифра: {SecondDigit}");
