19fdc813fb07cedf77b8af324833fc98579dfb72
zhyk <zhyk@list.ru>
2023-10-19 15:03:28 +0300

    Третья задача

---
+++ Task02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Выводит третью цифру заданного числа или сообщает
+// что третьей цифры нет.
+Console.Write("Введите любое число: ");
+string? number = Console.ReadLine();
+int length = number.Length;
+int numberint = Convert.ToInt32(number);
+if ((length < 3)&(numberint > 0) | (length < 4)&(numberint < 0))
+{
+    Console.Write("Третьей цифры нет.");
+}
+else
+{
+    if (numberint < 0)
+    {
+        Console.WriteLine($"Третья цифра {number[3]}");
+    }
+    else
+    {
+        Console.WriteLine($"Третья цифра {number[2]}");
+    }
+}
---
+++ Task03/Program.cs
@@ -0,0 +1,19 @@
+﻿// По номеру дня недели проверяет, является
+// ли этот день выходным. 
+Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number < 0) | (number > 7))
+{
+    Console.WriteLine("Это не день недели");
+}
+else
+{
+    if ((number == 6) | (number == 7))
+    {
+        Console.WriteLine("Это выходной");
+    }
+    else
+    {
+        Console.WriteLine("Это рабочий день");
+    }
+}
